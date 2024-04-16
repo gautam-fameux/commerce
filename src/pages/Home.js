156c4ee14a7392286bb9512af6e1f24937bd88b9
Gautam104 <gautamparmar4420@gmail.com>
2024-04-16 10:54:47 +0530

    second branch

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Navbar from "./Navbar"
-// import MainContent from "../components/MainContent/MainContent";
+import MainContent from "../components/MainContent/MainContent";
 // import Slide from "../components/slide/Slide";
 
 function Home(){
 return(
     <div>
      <Navbar/>
+     <MainContent/>
     </div>
 )
 }
